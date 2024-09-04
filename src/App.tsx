f7982d8aad5655f2f64b2e5ccc93772f22eaaafc
noel-pena <azulu88@hotmail.com>
2024-09-03 20:28:54 -0400

    init commit with starter files

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import TodayNotes from "./pages/TodayNotes";
+// import WeekNotes from "./pages/WeekNotes";
+// import MonthNotes from "./pages/MonthNotes";
+
+function App() {
+
+  return (
+    <>
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<TodayNotes />} />
+                {/*<Route path="/week" element={<WeekNotes />} />*/}
+                {/*<Route path="/month" element={<MonthNotes />} />*/}
+            </Routes>
+        </Router>
+    </>
+  )
+}
+
+export default App
